66eb009c9c33face2b0b4841daeee794ced4a260
Gabriel Parmer <gabep1@cs.bu.edu>
2008-04-07 18:33:46 -0400

    These should have been included in the previous commit: abstract away how we get stacks.

---
+++ module/user_prototype/linking/t/cos_asm_stacks.h
@@ -0,0 +1,16 @@
+#ifndef COS_ASM_STACKS_H
+#define COS_ASM_STACKS_H
+
+/*
+ * TODO: this could all be done without using eax.  If the thdid was
+ * pass in as ecx (thus esp as well given sysexit rules), these
+ * computations would require 2 instructions only manipulating esp.
+ * However, we would now need to pass in the spdid in eax, as ecx
+ * (where it is now) would be overloaded.
+ */
+#define COS_ASM_GET_STACK                   \
+	movl $cos_static_stack, %esp;	    \
+	shl $13, %eax;			    \
+	addl %eax, %esp;
+
+#endif
