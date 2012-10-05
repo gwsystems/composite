f643167db2cf24efb797338de17e2c5d4e4bb4bc
Xiang Gao <patriot7@live.cn>
2012-10-05 01:38:00 -0400

    backup

--- src/components/implementation/no_interface/fpu/f.c
+++ src/components/implementation/no_interface/fpu/f.c
@@ -1,16 +1,26 @@
 #include <cos_component.h>
 #include <print.h>
 #include <sched.h>
-
+#include <timed_blk.h>
 
 void cos_init(void *args)
 {
-	double f = 1;
+	double f = 1.5, g = 2.5, h;
 	int i;
+	timed_event_block(cos_spd_id(), 3);
+
+	h = f + g;
+	printc("test\n");
+	printc("%d\n", (int)h);
+	//printc("%lf\n", 0.0001);
 	for(i = 0; i<10; i++)
 	{
 		f *= 0.1;
-		printc("%lf\n", f);
+		//printc("%lf\n", f);
+	}
+	while (1) {
+		for (i = 0 ; i < 10000000 ; i++) ;
+		printc("%d", cos_get_thd_id());
 	}
 }
 
--- src/platform/linux/util/lfpu.sh
+++ src/platform/linux/util/lfpu.sh
@@ -2,11 +2,12 @@
 
 ./cos_loader \
 "c0.o, ;llboot.o, ;*fprr.o, ;mm.o, ;print.o, ;boot.o, ;\
-!fpu.o,a1;(!fpu1.o=fpu.o),a1:\
+!fpu.o,a1;(!fpu1.o=fpu.o),a1;!te.o, :\
 c0.o-llboot.o;\
 fprr.o-print.o|[parent_]mm.o|[faulthndlr_]llboot.o;\
 mm.o-[parent_]llboot.o|print.o;\
 boot.o-print.o|fprr.o|mm.o|llboot.o;\
-fpu.o-print.o|fprr.o|mm.o;\
-fpu1.o-print.o|fprr.o|mm.o\
+fpu.o-te.o|print.o|fprr.o|mm.o;\
+fpu1.o-te.o|print.o|fprr.o|mm.o;\
+te.o-print.o|fprr.o|mm.o\
 " ./gen_client_stub
