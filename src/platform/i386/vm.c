c93ad951f4fe7f0502420bf6be22d6eefa245231
Jakob Kaivo <jkaivo@gwu.edu>
2013-07-25 22:39:15 -0400

    Only create a single kernel page table (will later be a super table), only enable paging once

--- src/platform/i386/vm.c
+++ src/platform/i386/vm.c
@@ -6,7 +6,7 @@
 #include "vm.h"
 
 uint32_t pagedir[1024] __attribute__((aligned(4096)));
-uint32_t pagetab[1024][1024] __attribute__((aligned(4096)));
+uint32_t kernel_pagetab[1024] __attribute__((aligned(4096)));
 
 void *
 chal_pa2va(void *address)
@@ -23,17 +23,10 @@ chal_va2pa(void *address)
 static void
 load_page_directory(size_t dir)
 {
-    size_t cr0;
     uint32_t d = (uint32_t)chal_va2pa(pagedir) | PAGE_P;
      
-printk (INFO, "cr3 = %x (%x)\n", d, d);
+    printk (INFO, "Setting cr3 = %x (%x)\n", d, d);
     asm volatile("mov %0, %%cr3" : : "r"(d));
-    asm volatile("mov %%cr0, %0" : "=r"(cr0));
-printk (INFO, "cr0 == %x\n", cr0);
-    cr0 |= 0x80000000;
-printk (INFO, "cr0 = %x\n", cr0 | 0x80000000);
-    asm volatile("mov %0, %%cr0" : : "r"(cr0));
-printk (INFO, "OK\n");
 }
 
 static void
@@ -52,11 +45,11 @@ page_fault(struct registers *regs)
 
 }
 
-
 void
 paging__init(size_t memory_size)
 {
-    int i, j;
+    uint32_t cr0;
+    int i;
     printk(INFO, "Intialize paging\n");
     printk(INFO, "MEMORY_SIZE: %dMB\n", memory_size/1024);
 
@@ -65,17 +58,20 @@ paging__init(size_t memory_size)
 
     printk(INFO, "Mapping pages to tables and directories\n");
     for (i = 0; i < 1024; i++) {
-      pagedir[i] = (((uint32_t)pagetab[i] & PAGE_FRAME) | PAGE_RW | PAGE_P);
-      for (j = 0; j < 1024; j++) {
-        pagetab[i][j] = (((4096 * i * 1024) + (j * 4096)) & PAGE_FRAME) | PAGE_RW | (i == 0 ? PAGE_P : 0);
-      }
+        pagedir[i] = ((((uint32_t) &kernel_pagetab) + (i * 4096)) & PAGE_FRAME) | PAGE_RW | (i == 0 ? PAGE_P | PAGE_G : PAGE_US);
+        kernel_pagetab[i] = ((4096 * (i)) & PAGE_FRAME) | PAGE_RW | PAGE_P | PAGE_G;
     }
 
     printk(INFO, "Loading page directory\n");
-//printk(INFO, "pagetab == %x\n", pagetab);
-printk(INFO, "flags == %x\n", (PAGE_RW | PAGE_G) & (~ PAGE_US));
-printk(INFO, "pagedir[0] == %x\n", pagedir[0]);
     load_page_directory(0);
-    
+
+    printk(INFO, "Enabling paging\n");
+    asm volatile("mov %%cr0, %0" : "=r"(cr0));
+    printk (INFO, "cr0 == %x\n", cr0);
+    cr0 |= 0x80000000;
+    printk (INFO, "cr0 = %x\n", cr0 | 0x80000000);
+    asm volatile("mov %0, %%cr0" : : "r"(cr0));
+    printk (INFO, "OK\n");
+
     printk(INFO, "Finished\n");
 }
