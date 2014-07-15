b65585b8d1da8637a4334004f0e6454fecc19576
Gedare Bloom <gedare@rtems.org>
2014-07-15 11:53:42 -0400

    cstub: refactor cstub macros
    Simplify the macro interface. Also save and restore input registers
    around sysenter call.

--- src/components/interface/torrent/stubs/c_stub.c
+++ src/components/interface/torrent/stubs/c_stub.c
@@ -9,7 +9,13 @@ struct __sg_tsplit_data {
 	int len[2];
 	char data[0];
 };
-CSTUB_FN_ARGS_6(td_t, tsplit, spdid_t, spdid, td_t, tid, char *, param, int, len, tor_flags_t, tflags, long, evtid)
+
+CSTUB_FN(td_t, tsplit)(struct usr_inv_cap *uc,
+		spdid_t spdid, td_t tid, char * param,
+		int len, tor_flags_t tflags, long evtid)
+{
+	long fault = 0;
+	td_t ret;
 	struct __sg_tsplit_data *d;
 	cbuf_t cb;
 	int sz = len + sizeof(struct __sg_tsplit_data);
@@ -27,10 +33,11 @@ CSTUB_FN_ARGS_6(td_t, tsplit, spdid_t, spdid, td_t, tid, char *, param, int, len
         d->len[1] = len;
 	memcpy(&d->data[0], param, len + 1);
 
-CSTUB_ASM_3(tsplit, spdid, cb, sz)
+	CSTUB_INVOKE(ret, fault, uc, 3, spdid, cb, sz);
 
 	cbuf_free(cb);
-CSTUB_POST
+	return ret;
+}
 
 
 struct __sg_tmerge_data {
@@ -39,7 +46,12 @@ struct __sg_tmerge_data {
 	int len[2];
 	char data[0];
 };
-CSTUB_FN_ARGS_5(int, tmerge, spdid_t, spdid, td_t, td, td_t, td_into, char *, param, int, len)
+
+CSTUB_FN(int, tmerge)(struct usr_inv_cap *uc,
+		spdid_t spdid, td_t td, td_t td_into, char *param, int len)
+{
+	int ret;
+	long fault = 0;
 	struct __sg_tmerge_data *d;
 	cbuf_t cb;
 	int sz = len + sizeof(struct __sg_tmerge_data);
@@ -55,53 +67,57 @@ CSTUB_FN_ARGS_5(int, tmerge, spdid_t, spdid, td_t, td, td_t, td_into, char *, pa
         d->len[0] = 0;
         d->len[1] = len;
 	memcpy(&d->data[0], param, len);
-
-CSTUB_ASM_3(tmerge, spdid, cb, sz)
+	
+	CSTUB_INVOKE(ret, fault, uc, 3, spdid, cb, sz);
 
 	cbuf_free(cb);
-CSTUB_POST
+	return ret;
+}
 
-CSTUB_FN_ARGS_4(int, treadp, spdid_t, spdid, td_t, td, int *, off, int *, len)
-//	CSTUB_ASM_RET_PRE(*off, *len)
-	__asm__ __volatile__( \
-		"pushl %%ebp\n\t" \
-		"movl %%esp, %%ebp\n\t" \
-		"movl $1f, %%ecx\n\t" \
-		"sysenter\n\t" \
-		".align 8\n\t" \
-		"jmp 2f\n\t" \
-		".align 8\n\t" \
-		"1:\n\t" \
-		"popl %%ebp\n\t" \
-		"movl $0, %%ecx\n\t" \
-	        "movl %%esi, %%ebx\n\t" \
-	        "movl %%edi, %%edx\n\t" \
-		"jmp 3f\n\t" \
-		"2:\n\t" \
-		"popl %%ebp\n\t" \
-		"movl $1, %%ecx\n\t" \
-	        "movl %%esi, %%ebx\n\t" \
-	        "movl %%edi, %%edx\n\t" \
-		"3:" \
-	        : "=a" (ret), "=c" (fault), "=b" (*off), "=d" (*len)
-		: "a" (uc->cap_no), "b" (spdid), "S" (td)
-		: "edi", "memory", "cc");
-CSTUB_POST
+CSTUB_FN(int, treadp)(struct usr_inv_cap *uc,
+		spdid_t spdid, td_t td, int *off, int *sz)
+{
+	int ret;
+	long fault = 0;
+	CSTUB_INVOKE_3RETS(ret, fault, *off, *sz, uc, 2, spdid, td);
+	return ret;
+}
 
-CSTUB_4(int, tread, spdid_t, td_t, int, int);
-CSTUB_4(int, twrite, spdid_t, td_t, int, int);
+CSTUB_FN(int, tread)(struct usr_inv_cap *uc,
+		spdid_t spdid, td_t td, int cbid, int sz)
+{
+	int ret;
+	long fault = 0;
+	CSTUB_INVOKE(ret, fault, uc, 4, spdid, td, cbid, sz);
+	return ret;
+}
+
+CSTUB_FN(int, twrite)(struct usr_inv_cap *uc,
+		spdid_t spdid, td_t td, int cbid, int sz)
+{
+	int ret;
+	long fault = 0;
+	CSTUB_INVOKE(ret, fault, uc, 4, spdid, td, cbid, sz);
+	return ret;
+}
 
 struct __sg_trmeta_data {
         td_t td;
         int klen, retval_len;
         char data[0];
 };
-CSTUB_FN_ARGS_6(int, trmeta, spdid_t, spdid, td_t, td, const char *, key, unsigned int, klen, char *, retval, unsigned int, retval_len)
+
+CSTUB_FN(int, trmeta)(struct usr_inv_cap *uc,
+		spdid_t spdid, td_t td, const char *key,
+		unsigned int klen, char *retval, unsigned int max_rval_len)
+{
+	int ret;
+	long fault = 0;
         cbuf_t cb;
-        int sz = sizeof(struct __sg_trmeta_data) + klen + retval_len + 1;
+        int sz = sizeof(struct __sg_trmeta_data) + klen + max_rval_len + 1;
         struct __sg_trmeta_data *d;
 
-        assert(key && retval && klen > 0 && retval_len > 0);
+        assert(key && retval && klen > 0 && max_rval_len > 0);
         assert(key[klen] == '\0' && sz <= PAGE_SIZE);
 
         d = cbuf_alloc(sz, &cb);
@@ -109,28 +125,34 @@ CSTUB_FN_ARGS_6(int, trmeta, spdid_t, spdid, td_t, td, const char *, key, unsign
 
         d->td = td;
         d->klen = klen;
-        d->retval_len = retval_len;
+        d->retval_len = max_rval_len;
         memcpy(&d->data[0], key, klen + 1);
 
-CSTUB_ASM_3(trmeta, spdid, cb, sz)
+	CSTUB_INVOKE(ret, fault, uc, 3, spdid, cb, sz);
 
         if (ret >= 0) {
-                if ((unsigned int)ret > retval_len) { // as ret >= 0, cast it to unsigned int to omit compiler warning
+                if ((unsigned int)ret > max_rval_len) { // as ret >= 0, cast it to unsigned int to omit compiler warning
                         cbuf_free(cb);
                         return -EIO;
                 }
                 memcpy(retval, &d->data[klen + 1], ret + 1);
         }
         cbuf_free(cb);
-CSTUB_POST
-
+	return ret;
+}
 
 struct __sg_twmeta_data {
         td_t td;
         int klen, vlen;
         char data[0];
 };
-CSTUB_FN_ARGS_6(int, twmeta, spdid_t, spdid, td_t, td, const char *, key, unsigned int, klen, const char *, val, unsigned int, vlen)
+
+CSTUB_FN(int, twmeta)(struct usr_inv_cap *uc,
+		spdid_t spdid, td_t td, const char *key,
+		unsigned int klen, const char *val, unsigned int vlen)
+{
+	int ret;
+	long fault = 0;
         cbuf_t cb;
         int sz = sizeof(struct __sg_twmeta_data) + klen + vlen + 1;
         struct __sg_twmeta_data *d;
@@ -147,7 +169,8 @@ CSTUB_FN_ARGS_6(int, twmeta, spdid_t, spdid, td_t, td, const char *, key, unsign
         memcpy(&d->data[0], key, klen + 1);
         memcpy(&d->data[klen + 1], val, vlen + 1);
 
-CSTUB_ASM_3(twmeta, spdid, cb, sz)
+	CSTUB_INVOKE(ret, fault, uc, 3, spdid, cb, sz);
 
         cbuf_free(cb);
-CSTUB_POST
+	return ret;
+}
