f51bb00dbaf0ed188e03f5f45d1d995b2cac7941
Jakob Kaivo <jkaivo@gwu.edu>
2014-09-06 22:31:49 -0400

    Forgot to include in previous push

---
+++ src/platform/i386/qemu-g.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+if [ $# != 1 ]; then
+  echo "Usage: $0 <run-script.sh>"
+  exit 1
+fi
+
+if ! [ -r $1 ]; then
+  echo "Can't open run-script"
+  exit 1
+fi
+  
+TMPSCRIPT=/tmp/cos-rs-$$.sh
+TMPIMAGE=cos-image-$$.img
+
+
+sed -e "s/^\.\/cos_loader/\.\/cos_loader -o $TMPIMAGE/" $1 > $TMPSCRIPT
+sh $TMPSCRIPT
+rm $TMPSCRIPT
+
+qemu-system-i386 -m 128 -nographic -kernel kernel.img -no-reboot -s -S -initrd $TMPIMAGE
+
+rm $TMPIMAGE
+
