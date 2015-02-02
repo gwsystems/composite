2d847feda1fe4581348862afda2e5a67bd56ed79
gparmer <gparmer@gwu.edu>
2015-02-02 13:25:38 -0500

    BUGFIX commit: - Enabled the compilation of all components, which Jakob disabled. - Removed the compilation and cp of specific components that have suffered bitrot. - Added a qemu script that doesn't rely on the qemu-system-i386 option which doesn't exist on this system.

---
+++ src/platform/i386/qemu2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ $# != 1 ]; then
+  echo "Usage: $0 <run-script.sh>"
+  exit 1
+fi
+
+if ! [ -r $1 ]; then
+  echo "Can't open run-script"
+  exit 1
+fi
+
+MODULES=$(sh $1 | awk '/^Writing image/ { print $3; }' | tr '\n' ' ')
+
+qemu -m 128 -nographic -kernel kernel.img -no-reboot -s -initrd "$(echo $MODULES | tr ' ' ',')"
+
