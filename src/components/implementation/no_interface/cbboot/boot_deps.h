26f029456b4ab2b1266e5dda7d552970491becaf
Gedare Bloom <gedare@rtems.org>
2015-03-13 16:59:53 -0400

    cbboot: first attempt to implement component copying

--- src/components/implementation/no_interface/cbboot/boot_deps.h
+++ src/components/implementation/no_interface/cbboot/boot_deps.h
@@ -25,6 +25,7 @@
 
 COS_VECT_CREATE_STATIC(spd_sect_cbufs);
 COS_VECT_CREATE_STATIC(spd_sect_cbufs_header);
+COS_VECT_CREATE_STATIC(spd_sect_cbufs_mem);
 
 /* Need static storage for tracking cbufs to avoid dynamic allocation
  * before boot_deps_map_sect finishes. Each spd has probably 12 or so
@@ -40,6 +41,7 @@ boot_deps_init(void)
 {
 	cos_vect_init_static(&spd_sect_cbufs);
 	cos_vect_init_static(&spd_sect_cbufs_header);
+	cos_vect_init_static(&spd_sect_cbufs_mem);
 }
 
 static void
@@ -70,6 +72,7 @@ boot_deps_map_sect(spdid_t spdid, void *src_start, vaddr_t dest_start, int pages
 		all_cbufs_index += h->nsect;
 		if (cos_vect_add_id(&spd_sect_cbufs, sect_cbufs, spdid) < 0) BUG();
 		if (cos_vect_add_id(&spd_sect_cbufs_header, h, spdid) < 0) BUG();
+		if (cos_vect_add_id(&spd_sect_cbufs_mem, caddr, spdid) < 0) BUG();
 	}
 
 	assert(sect_cbufs);
@@ -93,4 +96,63 @@ boot_deps_save_hp(spdid_t spdid, void *hp)
 
 static void
 boot_deps_run(void) { return; }
+
+/* cbboot copying spds */
+static int 
+boot_spd_set_symbs(struct cobj_header *h, spdid_t spdid, struct cos_component_information *ci);
+
+spdid_t
+cbboot_copy(spdid_t spdid, spdid_t source)
+{
+	spdid_t d_spd = 0;
+	cbuf_t *sect_cbufs;
+	void *mem;
+	struct cobj_header *h;
+	struct cobj_sect *sect;
+	long tot = 0;
+	int j;
+
+	sect_cbufs = cos_vect_lookup(&spd_sect_cbufs, source);
+	h = cos_vect_lookup(&spd_sect_cbufs_header, source);
+	mem = cos_vect_lookup(&spd_sect_cbufs_mem, source);
+	if (!sect_cbufs || !h || !mem) BUG(); //goto done;
+	
+	/* copied partly from booter.c */
+	if ((d_spd = cos_spd_cntl(COS_SPD_CREATE, 0, 0, 0)) == 0) BUG();
+	sect = cobj_sect_get(h, 0);
+	if (cos_spd_cntl(COS_SPD_LOCATION, d_spd, sect->vaddr, SERVICE_SIZE)) BUG();
+	for (j = 0 ; j < (int)h->nsect ; j++) {
+		tot += cobj_sect_size(h, j);
+	}
+	if (tot > SERVICE_SIZE) {
+		if (cos_vas_cntl(COS_VAS_SPD_EXPAND, d_spd, sect->vaddr + SERVICE_SIZE, 
+				 3 * round_up_to_pgd_page(1))) {
+			printc("cbboot: could not expand VAS for component %d\n", h->id);
+			BUG();
+		}
+	}
+
+	for (j = 0 ; j < (int)h->nsect ; j++) {
+		vaddr_t d_addr;
+		cbuf_t cbid;
+		int flags;
+
+		sect = cobj_sect_get(h, j);
+		d_addr = sect->vaddr;
+		cbid = sect_cbufs[j];
+
+		if (sect->flags & COBJ_SECT_WRITE) flags = MAPPING_RW;
+		else flags = MAPPING_READ;
+		flags |= 2; /* no valloc */
+
+		if (d_addr != (cbuf_map_at(cos_spd_id(), cbid, d_spd, d_addr | flags))) BUG();
+		if (sect->flags & COBJ_SECT_CINFO) {
+			struct cos_component_information *ci = mem;
+			boot_spd_set_symbs(h, d_spd, ci);
+		}
+	}
+
+done:
+	return d_spd;
+}
 
