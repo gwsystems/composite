c548f5a26fcd30d8e64e72da4fb097ddd60f08cb
Gedare Bloom <gedare@rtems.org>
2015-08-10 11:14:35 -0400

    unit_cbboot_cbuf.sh: fix to load new ucbufpf

--- src/platform/linux/util/unit_cbboot_cbuf.sh
+++ src/platform/linux/util/unit_cbboot_cbuf.sh
@@ -3,7 +3,7 @@
 ./cos_loader \
 "c0.o, ;llboot.o, ;*fprr.o, ;mm.o, ;print.o, ;boot.o, ;\
 \
-!pfr.o,a2;!l.o,a1;!mpool.o,a3;!te.o,a3;!e.o,a4;!cbuf.o,a5;!va.o,a2;!vm.o,a1;!cbboot.o,a6;!ucbuf1.o,a10;!ucbuf2.o, ;!ucbufp.o,a9;!stat.o,a25:\
+!pfr.o,a2;!l.o,a1;!mpool.o,a3;!te.o,a3;!e.o,a4;!cbuf.o,a5;!va.o,a2;!vm.o,a1;!cbboot.o,a6;!ucbuf1.o,a10;!ucbuf2.o, ;!ucbufpf.o,a9;!stat.o,a25:\
 \
 c0.o-llboot.o;\
 print.o-llboot.o;\
@@ -20,9 +20,9 @@ mpool.o-print.o|fprr.o|mm.o|boot.o|va.o|l.o|llboot.o;\
 cbboot.o-print.o|fprr.o|mm.o|boot.o|cbuf.o|[parent_]llboot.o;\
 vm.o-fprr.o|print.o|mm.o|l.o|boot.o|llboot.o;\
 va.o-fprr.o|print.o|mm.o|l.o|boot.o|vm.o|llboot.o;\
-ucbuf1.o-fprr.o|ucbuf2.o|ucbufp.o|print.o|mm.o|va.o|cbuf.o|cbboot.o;\
+ucbuf1.o-fprr.o|ucbuf2.o|ucbufpf.o|print.o|mm.o|va.o|cbuf.o|cbboot.o;\
 ucbuf2.o-print.o|mm.o|va.o|cbuf.o|cbboot.o;\
-ucbufp.o-fprr.o|print.o|mm.o|va.o|cbuf.o|te.o|cbboot.o\
+ucbufpf.o-fprr.o|print.o|mm.o|va.o|cbuf.o|te.o|cbboot.o\
 " ./gen_client_stub
 
 #mpd.o-cbuf.o|cg.o|fprr.o|print.o|te.o|mm.o|va.o;\
