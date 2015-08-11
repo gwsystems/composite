4d4f0999f91772c84cb9e63c44c7dc269698f3d1
Gedare Bloom <gedare@rtems.org>
2015-08-11 15:56:07 -0400

    Fix lpp.sh runscript

--- src/platform/linux/util/lpp.sh
+++ src/platform/linux/util/lpp.sh
@@ -2,7 +2,7 @@
 
 ./cos_loader \
 "c0.o, ;llboot.o, ;*fprr.o, ;mm.o, ;print.o, ;boot.o, ;\
-!cpu.o, ;(!cpu1.o=cpu.o), ;(!po.o=ppong.o), ;(!pi.o=pingp.o), a9;!va.o, a2;!l.o,a1;!mpool.o, a3;!vm.o, a1:\
+!cpu.o, ;(!cpu1.o=cpu.o), ;(!po.o=ppong.o), ;(!pi.o=pingp.o), a9;!cbuf.o,a5;!va.o, a2;!l.o,a1;!mpool.o, a3;!vm.o, a1:\
 c0.o-llboot.o;\
 fprr.o-print.o|[parent_]mm.o|[faulthndlr_]llboot.o;\
 mm.o-[parent_]llboot.o|print.o;\
@@ -14,7 +14,7 @@ mpool.o-print.o|fprr.o|mm.o|boot.o|va.o|l.o;\
 vm.o-fprr.o|print.o|mm.o|l.o|boot.o;\
 va.o-fprr.o|print.o|mm.o|l.o|boot.o|vm.o;\
 l.o-fprr.o|mm.o|print.o;\
-\
+cbuf.o-boot.o|fprr.o|print.o|l.o|mm.o|va.o|llboot.o;\
 pi.o-cbuf.o|fprr.o|va.o|po.o|print.o;\
 po.o-cbuf.o|va.o|print.o\
 " ./gen_client_stub
