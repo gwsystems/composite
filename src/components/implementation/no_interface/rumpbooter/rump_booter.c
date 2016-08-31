4ed9e68ec645967caba666d12d72b843e5155e3b
Robert Gifford <robertgif@gmail.com>
2016-08-31 18:15:42 -0400

    Clean up qemu_rk.sh for booting and config files

--- src/components/implementation/no_interface/rumpbooter/rump_booter.c
+++ src/components/implementation/no_interface/rumpbooter/rump_booter.c
@@ -42,18 +42,23 @@ rump_booter_init(void)
 	extern int vmid;
 	printc("vmid: %d\n", vmid);
 
-	/* nginx */
-	//char *json_file = "{,\"blk\":{,\"source\":\"dev\",\"path\":\"/dev/paws\",\"fstype\":\"cd9660\",\"mountpoint\":\"data\",},\"net\":{,\"if\":\"vioif0\",\"type\":\"inet\",\"method\":\"static\",\"addr\":\"10.0.120.101\",\"mask\":\"24\",},\"net\":{,\"if\":\"tun0\",\"type\":\"inet\",\"method\":\"static\",\"addr\":\"111.111.111.0\",\"mask\":\"24\",\"gw\":\"111.111.111.0\",},\"cmdline\":\"nginx.bin\",},\0";
-
-	/* paws in qemu */
 	char *json_file = "";
+
+	/* nginx */
+	//json_file = "{,\"blk\":{,\"source\":\"dev\",\"path\":\"/dev/paws\",\"fstype\":\"cd9660\",\"mountpoint\":\"data\",},\"net\":{,\"if\":\"vioif0\",\"type\":\"inet\",\"method\":\"static\",\"addr\":\"10.0.120.101\",\"mask\":\"24\",},\"net\":{,\"if\":\"tun0\",\"type\":\"inet\",\"method\":\"static\",\"addr\":\"111.111.111.0\",\"mask\":\"24\",\"gw\":\"111.111.111.0\",},\"cmdline\":\"nginx.bin\",},\0";
+
+	/* paws in qemu
 	if(vmid == 0){
 		json_file = "{,\"net\":{,\"if\":\"vioif0\",\"type\":\"inet\",\"method\":\"static\",\"addr\":\"10.0.120.101\",\"mask\":\"24\",},\"cmdline\":\"paws.bin\",},\0";
 	}
+	*/
 
-	/* paws baremetal 
-	char *json_file = "{,\"net\":{,\"if\":\"wm0\",\"type\":\"inet\",\"method\":\"static\",\"addr\":\"192.168.0.2\",\"mask\":\"24\",},\"cmdline\":\"paws.bin\",},\0";
-*/
+	/* paws baremetal */
+	printc("~~~~~ vmid: %d ~~~~~\n");
+	if(vmid == 0) {
+		json_file = "{,\"net\":{,\"if\":\"wm0\",\"type\":\"inet\",\"method\":\"static\",\"addr\":\"192.168.0.2\",\"mask\":\"24\",},\"cmdline\":\"paws.bin\",},\0";
+	}
+
 
 	printc("\nRumpKernel Boot Start.\n");
 	cos2rump_setup();
