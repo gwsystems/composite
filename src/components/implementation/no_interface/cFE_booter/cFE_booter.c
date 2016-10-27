8214daac3727165eebec783dd6bbac0903885bc2
Gregor Peach <gregorpeach@gmail.com>
2016-10-27 16:10:45 -0400

    Implement printc for the cFE booter, and clean up its Makefile

--- src/components/implementation/no_interface/cFE_booter/cFE_booter.c
+++ src/components/implementation/no_interface/cFE_booter/cFE_booter.c
@@ -2,8 +2,8 @@
 #include <string.h>
 
 #include <cos_component.h>
-#include <cobj_format.h>
 #include <cos_kernel_api.h>
+#include <cobj_format.h>
 
 static void
 cos_llprint(char *s, int len)
@@ -19,6 +19,22 @@ prints(char *s)
 
 	return len;
 }
+
+int __attribute__((format(printf,1,2)))
+printc(char *fmt, ...)
+{
+	  char s[128];
+	  va_list arg_ptr;
+	  int ret, len = 128;
+
+	  va_start(arg_ptr, fmt);
+	  ret = vsnprintf(s, len, fmt, arg_ptr);
+	  va_end(arg_ptr);
+	  cos_llprint(s, ret);
+
+	  return ret;
+}
+
 
 void cos_init(void)
 {
