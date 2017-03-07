4ccb15ad98a045c113133cedb208159798517056
Teo Georgiev <tgeorgiev@gwu.edu>
2017-03-07 16:24:11 -0500

    Moving timer to replica (to be undone later)

--- src/components/implementation/fork_tests/voter/voter.c
+++ src/components/implementation/fork_tests/voter/voter.c
@@ -18,6 +18,7 @@ struct replica_info {
 	void *buf_write;
 	unsigned short int thread_id;
 	int blocked;
+	u64_t start;			// set each time the replica is woken up. Accurate?
 };
 
 struct nmod_comp {
@@ -27,7 +28,6 @@ struct nmod_comp {
 
 struct channel {
 	struct nmod_comp *snd, *rcv;
-	u64_t start;
 	int data, have_data;	// make these arrays
 };
 
@@ -91,7 +91,6 @@ int nwrite(spdid_t spdid, replica_type to, int data) {
 	else {
 		c->have_data = 1;
 		c->data = data;
-		rdtscll(c->start);							// set start time
 		ret = block_replica(replica);
 		if (ret < 0) printc("Error\n");
 		/* On wakeup */	
@@ -154,13 +153,30 @@ int confirm(spdid_t spdid, replica_type type) {
 	return 0;
 }
 
+void restart_comp(struct replica_info *replica) {
+	printc("About to fork but actually not implemented yet :(\n");
+}
+
 void monitor(void) {
-	struct nmod_comp *rcv_comp;
+	struct nmod_comp *rcv_comp, *component;
 	struct replica_info *rcv_replica;
 	int i, j;
 	int found = 0;
 	u64_t time;
 
+	/* Let's do waking up and restarting in a separate loop */
+	for (i = 0; i < N_COMPS; i++) {
+		component = &components[i];
+
+		for (j = 0; j < component->nreplicas; j++) {
+			rdtscll(time);
+			printc("time: %lu\n", time - component->replicas[j].start);
+			if (time > 500) {
+				restart_comp(&component->replicas[i]);
+			}
+		}
+	}
+	
 	for (i = 0; i < N_CHANNELS; i++) {
 		printc("Examing channel %d between nmodcomps with replicas %d and %d \n", i, channels[i].snd->replicas[0].spdid, channels[i].rcv->replicas[0].spdid);
 
@@ -172,9 +188,6 @@ void monitor(void) {
 		}
 
 		if (!found) continue;
-		rdtscll(time);
-
-		printc("time: %lu\n", time - channels[i].start);
 
 		printc("Examining receiving comp with replicas %d\n", rcv_comp->nreplicas);
 
@@ -183,10 +196,10 @@ void monitor(void) {
 
 			printc("Looking at replica %d\n", rcv_replica->spdid);
 
-			if (!rcv_replica->thread_id) BUG();
-
 			if (rcv_replica->blocked) {
+				rdtscll(rcv_replica->start);
 				rcv_replica->blocked = 0;
+				if (!rcv_replica->thread_id) BUG();
 				printc("waking up thread %d\n", rcv_replica->thread_id);
 				sched_wakeup(cos_spd_id(), rcv_replica->thread_id);
 			}
