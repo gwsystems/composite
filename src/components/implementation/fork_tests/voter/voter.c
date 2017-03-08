455be7eea2e18e808b18fb472d7e889107bce01d
Teo Georgiev <tgeorgiev@gwu.edu>
2017-03-08 12:36:41 -0500

    Getting rid of malloc

--- src/components/implementation/fork_tests/voter/voter.c
+++ src/components/implementation/fork_tests/voter/voter.c
@@ -18,17 +18,17 @@ struct replica_info {
 	void *buf_write;
 	unsigned short int thread_id;
 	int blocked;
-	u64_t start;			// set each time the replica is woken up. Accurate?
 };
 
 struct nmod_comp {
-	struct replica_info *replicas;
+	struct replica_info replicas[9];// let's call this max
 	int nreplicas;
 };
 
 struct channel {
 	struct nmod_comp *snd, *rcv;
 	int data, have_data;	// make these arrays
+	u64_t start;			// set each time the replica is woken up. Accurate?
 };
 
 struct map_entry {
@@ -121,6 +121,61 @@ int nread(spdid_t spdid, replica_type from, int data) {
 	}
 }
 
+void restart_comp(struct replica_info *replica) {
+	printc("About to fork but actually not implemented yet :(\n");
+}
+
+void monitor(void) {
+	struct nmod_comp *rcv_comp;
+	struct channel *channel;
+	struct replica_info *rcv_replica;
+	int i, j;
+	int found = 0;
+	u64_t time;
+
+	/* Let's do waking up and restarting in a separate loop */
+	for (i = 0; i < N_CHANNELS; i++) {
+		channel = &channels[i];
+
+		for (j = 0; j < channel->rcv->nreplicas; j++) {
+			rdtscll(time);
+			printc("time: %lu\n", time - channel->start);
+			if (time > 500) {					// this is waaaaaay too small of a timeout. How many cycles are 10 seconds???
+				restart_comp(&channel->rcv->replicas[i]);
+			}
+		}
+	}
+	
+	for (i = 0; i < N_CHANNELS; i++) {
+		printc("Examing channel %d between nmodcomps with replicas %d and %d \n", i, channels[i].snd->replicas[0].spdid, channels[i].rcv->replicas[0].spdid);
+
+		printc("channel data present %d (%d)\n", channels[i].have_data, channels[i].data);
+		rdtscll(channels[i].start);
+
+		if (channels[i].have_data) {
+			rcv_comp = channels[i].rcv;
+			found = 1;
+		}
+
+		if (!found) continue;
+
+		printc("Examining receiving comp with replicas %d\n", rcv_comp->nreplicas);
+
+		for (j = 0; j < rcv_comp->nreplicas; j++) {
+			rcv_replica = &rcv_comp->replicas[j];
+
+			printc("Looking at replica %d\n", rcv_replica->spdid);
+
+			if (rcv_replica->blocked) {
+				rcv_replica->blocked = 0;
+				if (!rcv_replica->thread_id) BUG();
+				printc("waking up thread %d\n", rcv_replica->thread_id);
+				sched_wakeup(cos_spd_id(), rcv_replica->thread_id);
+			}
+		}
+	}
+}
+
 int confirm(spdid_t spdid, replica_type type) {
 	printc("Confirming readiness of spdid %d\n", spdid);
 	
@@ -153,67 +208,12 @@ int confirm(spdid_t spdid, replica_type type) {
 	return 0;
 }
 
-void restart_comp(struct replica_info *replica) {
-	printc("About to fork but actually not implemented yet :(\n");
-}
-
-void monitor(void) {
-	struct nmod_comp *rcv_comp, *component;
-	struct replica_info *rcv_replica;
-	int i, j;
-	int found = 0;
-	u64_t time;
-
-	/* Let's do waking up and restarting in a separate loop */
-	for (i = 0; i < N_COMPS; i++) {
-		component = &components[i];
-
-		for (j = 0; j < component->nreplicas; j++) {
-			rdtscll(time);
-			printc("time: %lu\n", time - component->replicas[j].start);
-			if (time > 500) {
-				restart_comp(&component->replicas[i]);
-			}
-		}
-	}
-	
-	for (i = 0; i < N_CHANNELS; i++) {
-		printc("Examing channel %d between nmodcomps with replicas %d and %d \n", i, channels[i].snd->replicas[0].spdid, channels[i].rcv->replicas[0].spdid);
-
-		printc("channel data present %d (%d)\n", channels[i].have_data, channels[i].data);
-
-		if (channels[i].have_data) {
-			rcv_comp = channels[i].rcv;
-			found = 1;
-		}
-
-		if (!found) continue;
-
-		printc("Examining receiving comp with replicas %d\n", rcv_comp->nreplicas);
-
-		for (j = 0; j < rcv_comp->nreplicas; j++) {
-			rcv_replica = &rcv_comp->replicas[j];
-
-			printc("Looking at replica %d\n", rcv_replica->spdid);
-
-			if (rcv_replica->blocked) {
-				rdtscll(rcv_replica->start);
-				rcv_replica->blocked = 0;
-				if (!rcv_replica->thread_id) BUG();
-				printc("waking up thread %d\n", rcv_replica->thread_id);
-				sched_wakeup(cos_spd_id(), rcv_replica->thread_id);
-			}
-		}
-	}
-}
-
 void cos_init(void)
 {
 	int i, j;
 	for (i = 0; i < N_COMPS; i++) {
 		printc("Setting up nmod component %d\n", i);
 		components[i].nreplicas = 1; // for now, no fork
-		components[i].replicas = (struct replica_info*) malloc(sizeof(struct replica_info) * components[i].nreplicas);
 	
 		for (j = 0; j < components[i].nreplicas; j++) {
 			components[i].replicas[j].spdid = 0;
