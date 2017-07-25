1626bb0c6e050ea6c23a737a2307a0c083296919
Vlad Nitu <vlad.nitu@enseeiht.fr>
2017-07-25 19:06:58 -0400

    Passed unit test for c++. Increased MAX_SYMBOLS to 1024*64. integrated c++ compilation in COS make infrastructure.
    Signed-off-by: Vlad Nitu <vlad.nitu@enseeiht.fr>

---
+++ src/components/implementation/tests/unit_cxx/init.c
@@ -0,0 +1,13 @@
+#include <cos_component.h>
+#include <cobj_format.h>
+#include <cos_kernel_api.h>
+#include "init.h"
+
+struct cos_compinfo parent_cinfo;
+
+void
+init_cinfo(void){
+	cos_meminfo_init(&parent_cinfo.mi, BOOT_MEM_KM_BASE, COS_MEM_KERN_PA_SZ, BOOT_CAPTBL_SELF_UNTYPED_PT);
+	cos_compinfo_init(&parent_cinfo, BOOT_CAPTBL_SELF_PT, BOOT_CAPTBL_SELF_CT, BOOT_CAPTBL_SELF_COMP,
+					(vaddr_t)cos_get_heap_ptr(), BOOT_CAPTBL_FREE, &parent_cinfo);
+}
---
+++ src/components/implementation/tests/unit_cxx/init.h
@@ -0,0 +1,6 @@
+#ifndef CXX_UNIT_INIT_H
+#define CXX_UNIT_INIT_H
+
+void init_cinfo(void);
+
+#endif /* CXX_UNIT_INIT_H */
