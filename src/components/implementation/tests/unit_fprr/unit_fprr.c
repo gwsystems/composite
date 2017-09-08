a1a0ea160a43d629906006aa360014528f8242fc
Gregor Peach <gregorpeach@gmail.com>
2017-09-08 11:28:34 -0400

    Cleanup fprr a little more, and fix style in the tests

--- src/components/implementation/tests/unit_fprr/unit_fprr.c
+++ src/components/implementation/tests/unit_fprr/unit_fprr.c
@@ -17,83 +17,95 @@
 #define LOW_PRIORITY (LOWEST_PRIORITY - 1)
 #define HIGH_PRIORITY (LOWEST_PRIORITY - 10)
 
-int lowest_was_scheduled = 0;
+static int lowest_was_scheduled = 0;
 
-void
+static void
 low_thread_fn()
 {
 	lowest_was_scheduled = 1;
-	sl_thd_free(sl_thd_curr());
+	sl_thd_exit();
 }
 
-void
+static void
 high_thread_fn()
 {
-	struct sl_thd *low_thread = sl_thd_alloc(low_thread_fn, NULL);
+	struct sl_thd *low_thread;
+	cycles_t deadline;
+
+	low_thread = sl_thd_alloc(low_thread_fn, NULL);
 	sl_thd_param_set(low_thread, sched_param_pack(SCHEDP_PRIO, LOW_PRIORITY));
 
-	cycles_t deadline = sl_now() + sl_usec2cyc(10 * 1000 * 1000);
+	deadline = sl_now() + sl_usec2cyc(10 * 1000 * 1000);
 	while (sl_now() < deadline) {}
 	assert(!lowest_was_scheduled);
-	sl_thd_free(sl_thd_curr());
+	sl_thd_exit();
 }
 
-void
+static void
 test_highest_is_scheduled(void)
 {
-	struct sl_thd *high_thread = sl_thd_alloc(high_thread_fn, NULL);
+	struct sl_thd *high_thread;
+	cycles_t wakeup;
+
+	high_thread = sl_thd_alloc(high_thread_fn, NULL);
 	sl_thd_param_set(high_thread, sched_param_pack(SCHEDP_PRIO, HIGH_PRIORITY));
 
-	cycles_t wakeup = sl_now() + sl_usec2cyc(1000 * 1000);
+	wakeup = sl_now() + sl_usec2cyc(1000 * 1000);
 	sl_thd_block_timeout(0, wakeup);
 }
 
-int thd1_ran = 0;
-int thd2_ran = 0;
+static int thd1_ran = 0;
+static int thd2_ran = 0;
 
-void
+static void
 thd1_fn()
 {
 	thd1_ran = 1;
-	while(1);
+	while (1);
 }
 
-void
+static void
 thd2_fn()
 {
 	thd2_ran = 1;
-	while(1);
+	while (1);
 }
 
-void
+static void
 allocator_thread_fn()
 {
-	struct sl_thd *thd1 = sl_thd_alloc(thd1_fn, NULL);
+	struct sl_thd *thd1, *thd2;
+	cycles_t wakeup;
+
+	thd1 = sl_thd_alloc(thd1_fn, NULL);
 	sl_thd_param_set(thd1, sched_param_pack(SCHEDP_PRIO, LOW_PRIORITY));
 
-	struct sl_thd *thd2 = sl_thd_alloc(thd2_fn, NULL);
+	thd2 = sl_thd_alloc(thd2_fn, NULL);
 	sl_thd_param_set(thd2, sched_param_pack(SCHEDP_PRIO, LOW_PRIORITY));
 
-	cycles_t wakeup = sl_now() + sl_usec2cyc(1000 * 1000);
+	wakeup = sl_now() + sl_usec2cyc(1000 * 1000);
 	sl_thd_block_timeout(0, wakeup);
 
 	sl_thd_free(thd1);
 	sl_thd_free(thd2);
 
-	sl_thd_free(sl_thd_curr());
+	sl_thd_exit();
 }
 
-void
+static void
 test_swapping(void)
 {
-	struct sl_thd *allocator_thread = sl_thd_alloc(allocator_thread_fn, NULL);
+	struct sl_thd *allocator_thread;
+	cycles_t wakeup;
+
+	allocator_thread = sl_thd_alloc(allocator_thread_fn, NULL);
 	sl_thd_param_set(allocator_thread, sched_param_pack(SCHEDP_PRIO, HIGH_PRIORITY));
 
-	cycles_t wakeup = sl_now() + sl_usec2cyc(100 * 1000);
+	wakeup = sl_now() + sl_usec2cyc(100 * 1000);
 	sl_thd_block_timeout(0, wakeup);
 }
 
-void
+static void
 run_tests()
 {
 	test_highest_is_scheduled();
@@ -101,13 +113,14 @@ run_tests()
 	test_swapping();
 	printc("Test successful! We swapped back and forth!\n");
 
-	printc("Done testing, faulting...\n");
-	assert(0);
+	printc("Done testing, spinning...\n");
+	SPIN();
 }
 
 void
 cos_init(void)
 {
+	struct sl_thd *testing_thread;
 	struct cos_defcompinfo *defci = cos_defcompinfo_curr_get();
 	struct cos_compinfo *   ci    = cos_compinfo_get(defci);
 
@@ -116,7 +129,7 @@ cos_init(void)
 	cos_defcompinfo_init();
 	sl_init();
 
-	struct sl_thd *testing_thread = sl_thd_alloc(run_tests, NULL);
+	testing_thread = sl_thd_alloc(run_tests, NULL);
 	sl_thd_param_set(testing_thread, sched_param_pack(SCHEDP_PRIO, LOWEST_PRIORITY));
 
 	sl_sched_loop();
