1896a7af01dfa9e21c9a6d054c44c265e79a9035
phani <phanikishoreg@gwu.edu>
2017-09-23 13:14:47 -0400

    setting timeout when switching to the scheduler is unnecessary

--- src/components/lib/sl/sl.c
+++ src/components/lib/sl/sl.c
@@ -31,7 +31,8 @@ sl_cs_enter_contention(union sl_cs_intern *csi, union sl_cs_intern *cached, thdc
 		if (!ps_cas(&g->lock.u.v, cached->v, csi->v)) return 1;
 	}
 	/* Switch to the owner of the critical section, with inheritance using our tcap/priority */
-	if ((ret = cos_defswitch(csi->s.owner, t->prio, g->timeout_next, tok))) return ret;
+	if ((ret = cos_defswitch(csi->s.owner, t->prio, csi->s.owner == sl_thd_thdcap(g->sched_thd) ? 
+				 TCAP_TIME_NIL : g->timeout_next, tok))) return ret;
 	/* if we have an outdated token, then we want to use the same repeat loop, so return to that */
 
 	return 1;
@@ -46,7 +47,7 @@ sl_cs_exit_contention(union sl_cs_intern *csi, union sl_cs_intern *cached, sched
 
 	if (!ps_cas(&g->lock.u.v, cached->v, 0)) return 1;
 	/* let the scheduler thread decide which thread to run next, inheriting our budget/priority */
-	cos_defswitch(g->sched_thdcap, t->prio, g->timeout_next, tok);
+	cos_defswitch(g->sched_thdcap, t->prio, TCAP_TIME_NIL, tok);
 
 	return 0;
 }
