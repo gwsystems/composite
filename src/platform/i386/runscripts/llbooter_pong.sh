46bb61ec5daeee3e0aa8405ac32968566accf4bf
lab176 <lbaier176@gmail.com>
2017-11-12 14:33:57 -0500

    Add ping_two component
    Adds test for two client components invoking the same server

--- src/platform/i386/runscripts/llbooter_pong.sh
+++ src/platform/i386/runscripts/llbooter_pong.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cp llboot_test.o llboot.o
-./cos_linker "llboot.o, ;ppong.o, ;pingp.o, :pingp.o-ppong.o" ./gen_client_stub
+./cos_linker "llboot.o, ;ppong.o, ;pingp.o, ;pingp_two.o, :pingp.o-ppong.o;pingp_two.o-ppong.o" ./gen_client_stub
