a84183ff31485d3881980f202c25826a65f3553c
lab176 <lbaier176@gmail.com>
2018-01-20 15:54:13 -0500

    Add shdmem component to iot setup

--- src/platform/i386/runscripts/gateway.sh
+++ src/platform/i386/runscripts/gateway.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cp llboot_test.o llboot.o
-./cos_linker "llboot.o, ;robot_controller.o, ;robot_scheduler.o, :robot_scheduler.o-robot_controller.o" ./gen_client_stub
+./cos_linker "llboot.o, ;shmem.o, ;robot_controller.o, ;robot_scheduler.o, :robot_controller.o-shmem.o;robot_scheduler.o-robot_controller.o" ./gen_client_stub
