d5b9acd7860415f38a71bf47454c24533e157635
644580703@qq.com <644580703@qq.com>
2018-02-27 10:51:25 -0500

    changes merged & fixed compilation error

--- src/platform/i386/chal_pgtbl.h
+++ src/platform/i386/chal_pgtbl.h
@@ -1,27 +1,6 @@
 #ifndef CHAL_PGTBL_H
 #define CHAL_PGTBL_H
 
-/* Generic definitions */
-typedef enum {
-	PGTBL_PRESENT  = 1,
-	PGTBL_WRITABLE = 1 << 1,
-	PGTBL_USER     = 1 << 2,
-	PGTBL_WT       = 1 << 3, /* write-through caching */
-	PGTBL_NOCACHE  = 1 << 4, /* caching disabled */
-	PGTBL_ACCESSED = 1 << 5,
-	PGTBL_MODIFIED = 1 << 6,
-	PGTBL_SUPER    = 1 << 7, /* super-page (4MB on x86-32) */
-	PGTBL_GLOBAL   = 1 << 8,
-	/* Composite defined bits next*/
-	PGTBL_COSFRAME   = 1 << 9,
-	PGTBL_COSKMEM    = 1 << 10, /* page activated as kernel object */
-	PGTBL_QUIESCENCE = 1 << 11,
-	/* Flag bits done. */
-
-	PGTBL_USER_DEF   = PGTBL_PRESENT | PGTBL_USER | PGTBL_ACCESSED | PGTBL_MODIFIED | PGTBL_WRITABLE,
-	PGTBL_INTERN_DEF = PGTBL_USER_DEF,
-} pgtbl_flags_t;
-
 /* These code below are for x86 specifically, only used in x86 chal */
 typedef enum {
 	X86_PRESENT    = 1,
