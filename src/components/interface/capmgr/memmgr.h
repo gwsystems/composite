b8ae505077590edb84c103b62b4b300484fc0170
Robert Gifford <robertgif@gmail.com>
2018-03-27 22:00:19 -0400

    Cherry-pick tls extension to memmgr

--- src/components/interface/capmgr/memmgr.h
+++ src/components/interface/capmgr/memmgr.h
@@ -10,4 +10,23 @@ int memmgr_shared_page_alloc(vaddr_t *pgaddr);
 int memmgr_shared_page_allocn(int num_pages, vaddr_t *pgaddr);
 int memmgr_shared_page_map(int id, vaddr_t *pgaddr);
 
+/* This magic number is double the tls size defined in RK */
+#define TLS_AREA_SIZE 32
+#define TLS_NUM_PAGES (round_up_to_page(TLS_AREA_SIZE * MAX_NUM_THREADS) / PAGE_SIZE)
+#define TLS_BASE_ADDR 0x70000000
+
+void *memmgr_tls_alloc(unsigned int dst_tid);
+void *_memmgr_tls_alloc_and_set(void *area);
+
+static void *
+memmgr_tls_alloc_and_set(void *area)
+{
+	void *addr = _memmgr_tls_alloc_and_set(area);
+
+	/* Set area within addr for this tid */
+	*(void **)addr = area;
+
+	return addr;
+}
+
 #endif /* MEMMGR_H */
