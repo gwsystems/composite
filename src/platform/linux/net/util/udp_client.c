282ccc068dae37f88ab4ce386588a6caaab0ec49
lab176 <lbaier176@gmail.com>
2018-04-16 14:52:36 -0400

    Fix Jpeg request from camera
    Much cleaner solution to jpeg request, uses avconv directly from client
    Doesn't rely on background processes/applications

--- src/platform/linux/net/util/udp_client.c
+++ src/platform/linux/net/util/udp_client.c
@@ -15,8 +15,6 @@
 #include <signal.h>
 #include <time.h>
 
-#include "genjpeg.h"
-
 #define rdtscll(val) \
         __asm__ __volatile__("rdtsc" : "=A" (val))
 
@@ -105,6 +103,12 @@ build_script()
 //	printf("str: %s \n", str);
 //}
 
+void
+req_jpeg(void)
+{
+        system("avconv -rtsp_transport udp -i 'rtsp://192.168.248.102:554/onvif1' -f image2 -vframes 1 -pix_fmt yuvj420p hey.jpg");
+}
+
 int 
 send_script() 
 {
@@ -152,9 +156,8 @@ unsigned char * buf;
 int
 read_jpeg(void)
 {
-	createjpeg();
 	printf("reading in jpeg\n");
-	fp = fopen("obstacleman.jpg", "rb");
+	fp = fopen("hey.jpg", "rb");
 	assert(fp);
 	
 	fseek(fp, 0, SEEK_END);
@@ -181,6 +184,7 @@ main(int argc, char *argv[])
 	char *msg;
 	int sleep_val;
 
+	req_jpeg();
 	if (argc != 5 && argc != 6) {
 		printf("Usage: %s <ip> <port> <msg size> <sleep_val> <opt:rcv_port>\n", argv[0]);
 		return -1;
@@ -246,7 +250,7 @@ main(int argc, char *argv[])
 		/* Server has requested an image */
 		if (((unsigned char *)rcv_msg)[0] == JPEG_REQ) {
 			printf("Server requested an image\n");	
-			read_jpeg();
+			req_jpeg();
 
 			((unsigned int *)msg)[0] = 79;			
 			if (sendto(fd, msg, msg_size, 0, (struct sockaddr*)&sa, sizeof(sa)) < 0 &&
