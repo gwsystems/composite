eb01119a166664b12bec7a556c0993ea2669a235
Gabe Parmer <gparmer@gwu.edu>
2018-07-06 11:31:29 -0400

    mkimg: updated the component acquisition to use the actual component hierarchy, rather than locally copied versions.
    - remove the local .o files
    - update the minimal test file to use <if>.<compname> to identify the component
    - translate that component specification into `src/components/implementation/if/compname/if.compname.o`

--- src/mkimg/src/compose.rs
+++ src/mkimg/src/compose.rs
@@ -2,9 +2,11 @@ use syshelpers::{dump_file, emit_file, reset_dir, exec_pipeline};
 use cossystem::{CosSystem};
 use compobject::CompObject;
 use std::collections::BTreeMap;
+use std::env;
 
 pub struct ComposeSpec {
     sysspec: String,
+    src_path: String,
     sys: CosSystem,
     binaries: BTreeMap<String, Vec<u8>>,
 }
@@ -18,15 +20,34 @@ impl ComposeSpec {
     pub fn parse_spec(sysspec: String) -> Result<ComposeSpec, String> {
         let sys = CosSystem::parse(sysspec.clone())?;
         let mut bins = BTreeMap::new();
+        let mut path = String::from(env!("PWD"));
+
+        // Construct the path to the src directory
+        path.push_str("/../");
 
         for c in sys.comps().iter() {
-            let obj_contents = dump_file(&c.img())?;
+            let mut obj_path = String::new();
+            let mut obj_str = c.img().clone();
+            obj_path.push_str(&path);
+            obj_path.push_str("components/implementation/");
+
+            let mut obj_subpath = obj_str.replace(".", "/");
+            obj_subpath.push_str("/");
+            obj_path.push_str(&obj_subpath);
+
+            obj_path.push_str(&obj_str);
+            obj_path.push_str(".o");
+
+            println!("{}", obj_path);
+
+            let obj_contents = dump_file(&obj_path)?;
 
             bins.insert(c.name().clone(), obj_contents);
         }
 
         Ok(ComposeSpec {
             sysspec: sysspec,
+            src_path: path,
             sys: sys,
             binaries: bins,
         })
