0c75f1d6767bdc55ddff63207c460fe320ad2858
phani <phanikishoreg@gwu.edu>
2019-04-29 12:13:25 -0400

    ICV api in cos_omp and move cos_gomp to directory

--- src/components/include/cos_omp.h
+++ src/components/include/cos_omp.h
@@ -13,6 +13,37 @@
 
 #define COS_OMP_MAX_NUM_THREADS (NUM_CPU)
 
+struct cos_icv_data_env {
+	unsigned dyn_var;
+	unsigned nest_var;
+	unsigned nthreads_var;
+	unsigned run_sched_var;
+	unsigned bind_var;
+	unsigned thread_limit_var;
+	unsigned active_levels_var;
+	unsigned levels_var;
+	unsigned default_device_var;
+};
+
+struct cos_icv_global_env {
+	unsigned cancel_var;
+	unsigned max_task_priority_var;
+};
+
+struct cos_icv_implicittask_env {
+	unsigned place_partition_var;
+};
+
+struct cos_icv_device_env {
+	unsigned def_sched_var;
+	unsigned stacksize_var;
+	unsigned wait_policy_var;
+	unsigned max_active_levels_var;
+};
+
+extern void cos_omp_icv_data_init(struct cos_icv_data_env *icvde);
+extern void cos_omp_icv_implitsk_init(struct cos_icv_implicittask_env *icvite);
+extern void cos_omp_icv_device_init(struct cos_icv_device_env *icvdve, unsigned dev_no);
 extern void cos_omp_init(void);
 
 #endif /* COS_OMP_H */
--- src/components/lib/cos_omp.c
+++ src/components/lib/cos_omp.c
@@ -9,6 +9,13 @@
 #include <cos_kernel_api.h>
 #include <cos_types.h>
 
+#define COS_OMP_NUM_DEVS 1
+
+static struct cos_icv_global_env       cos_icv_glbenv;
+static struct cos_icv_device_env       cos_icv_devenv[COS_OMP_NUM_DEVS];
+static struct cos_icv_data_env         cos_icv_init_dataenv;
+static struct cos_icv_implicittask_env cos_icv_init_implitskenv;
+static unsigned int _cos_omp_init_done = 0;
 static unsigned int _cycs_per_usec = 0;
 
 #define _USEC_TO_SEC_d(x) (((double)x)/(double)(1000*1000))
@@ -57,10 +64,76 @@ omp_get_thread_num(void)
 	return (cos_thdid() % omp_get_max_threads());
 }
 
+static inline void
+cos_omp_icv_global_init(void)
+{
+	assert(!_cos_omp_init_done);
+	/* TODO: what is not int? what is not zero? */
+	/* cos_icv_glbenv.xxxx = yyyy; */
+}
+
+void
+cos_omp_icv_data_init(struct cos_icv_data_env *icvde)
+{
+	if (unlikely(icvde == &cos_icv_init_dataenv)) {
+		assert(!_cos_omp_init_done); /* init only on startup! */
+
+		/* TODO: what is not int? what is not zero! */
+		return;
+	}
+
+	assert(_cos_omp_init_done);
+	memcpy(icvde, &cos_icv_init_dataenv, sizeof(struct cos_icv_data_env));
+}
+
+void
+cos_omp_icv_implitsk_init(struct cos_icv_implicittask_env *icvite)
+{
+	if (unlikely(icvite == &cos_icv_init_implitskenv)) {
+		assert(!_cos_omp_init_done); /* init only on startup! */
+
+		/* TODO: what is not int? what is not zero! */
+		return;
+	}
+
+	assert(_cos_omp_init_done);
+	memcpy(icvite, &cos_icv_init_implitskenv, sizeof(struct cos_icv_implicittask_env));
+}
+
+void
+cos_omp_icv_device_init(struct cos_icv_device_env *icvdve, unsigned dev_no)
+{
+	assert(dev_no < COS_OMP_NUM_DEVS);
+
+	if (unlikely(icvdve == &cos_icv_devenv[dev_no])) {
+		assert(!_cos_omp_init_done); /* init only on startup! */
+
+		/* TODO: what is not int? what is not zero! */
+		return;
+	}
+
+	assert(_cos_omp_init_done);
+	memcpy(icvdve, &cos_icv_devenv[dev_no], sizeof(struct cos_icv_device_env));
+}
+
+static inline void
+cos_omp_icv_init(void)
+{
+	cos_omp_icv_global_init();
+
+	cos_omp_icv_device_init(&cos_icv_devenv[0], 0);
+
+	cos_omp_icv_data_init(&cos_icv_init_dataenv);
+	cos_omp_icv_implitsk_init(&cos_icv_init_implitskenv);
+}
+
 void
 cos_omp_init(void)
 {
 	_cycs_per_usec = cos_hw_cycles_per_usec(BOOT_CAPTBL_SELF_INITHW_BASE);
 
 	assert(_cycs_per_usec);
+
+	cos_omp_icv_init();
+	_cos_omp_init_done = 1;
 }
