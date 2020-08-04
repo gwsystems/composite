e0369986cc3ec4620d8edc14e92ae92b0b6e6e9f
phani <phanikishoreg@gwu.edu>
2020-08-03 23:32:42 -0400

    remove old musl and use new one

--- src/components/lib/libc/musl-1.1.11/src/thread/clone.c
+++
@@ -1,9 +0,0 @@
-#include <errno.h>
-#include <stdarg.h>
-#include <syscall.h>
-#include "pthread_impl.h"
-
-int __clone(int (*a)(void *), void *b, int c, void *d, void* e, void* f, void* g)
-{
-	return (int) __cos_syscall(__NR_clone, (long)a, (long)b, (long)c, (long)d, (long)e, (long)f, (long)g);
-}
