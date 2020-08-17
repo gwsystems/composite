c15c636d151e94ecaf82b1c82f0d4204ae1495f6
phani <phanikishoreg@gwu.edu>
2020-08-17 18:02:28 -0400

    inline memcpy n memset on x86

--- src/platform/i386/string.c
+++ src/platform/i386/string.c
@@ -3,27 +3,6 @@
 
 static char *str_buf;
 
-void *
-memcpy(void *dst, const void *src, size_t count)
-{
-	const u8_t *s = (const u8_t *)src;
-	u8_t *      d = (u8_t *)dst;
-
-	for (; count != 0; count--) *d++ = *s++;
-
-	return dst;
-}
-
-void *
-memset(void *dst, int c, size_t count)
-{
-	char *p = (char *)dst;
-
-	for (; count != 0; count--) *p++ = c;
-
-	return dst;
-}
-
 size_t
 strnlen(const char *str, size_t max)
 {
