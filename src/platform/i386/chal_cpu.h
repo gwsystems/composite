2b82dc825ce173cc932cdfb9e0672b955bdae3d4
betahxy <betahxy>
2021-09-05 03:55:08 -0400

    disable sse in kernel space and setup iopl in user space

--- src/platform/i386/chal_cpu.h
+++ src/platform/i386/chal_cpu.h
@@ -172,7 +172,8 @@ chal_user_upcall(void *ip, u16_t tid, u16_t cpuid)
 {
 #if defined(__x86_64__)
 	/* rcx = user-level ip, r12 = option, rbx = arg, rax = tid + cpuid  */
-	__asm__("movq $0x200, %%r11 ; mov %%rdx, %%ds ; movq %3, %%r12 ; sysretq" : : "c"(ip), "a"(tid | (cpuid << 16)), "d"(SEL_UDSEG), "i"(0), "b"(0));
+	/* $0x3200 : enable interrupt, and iopl is set to 3, the same as user's CPL */
+	__asm__("movq $0x3200, %%r11 ; mov %%rdx, %%ds ; movq %3, %%r12 ; sysretq" : : "c"(ip), "a"(tid | (cpuid << 16)), "d"(SEL_UDSEG), "i"(0), "b"(0));
 #elif defined(__i386__)
 	/* edx = user-level ip, ecx = option, ebx = arg, eax = tid + cpuid */
 	__asm__("sti ; sysexit" : : "c"(0), "d"(ip), "b"(0), "a"(tid | (cpuid << 16)));
