0f7ea1a0f74c5e395c27a28589d64291969e955a
betahxy <55125653+betahxy@users.noreply.github.com>
2021-10-06 00:27:27 -0400

    fix Makefiel system in components/Makefile.comp

--- gen_cos_img.sh
+++ gen_cos_img.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-rm /home/user/projects/composite/system_binaries/cos_build-test/cos.img
-rm /home/user/projects/composite/system_binaries/cos_build-test/constructor
-rm /home/user/projects/composite/system_binaries/cos_build-test/global.tests/tests.kernel_tests.global.tests
+rm /home/user/projects/composite/system_binaries/cos_build-ktest/cos.img
+rm /home/user/projects/composite/system_binaries/cos_build-ktest/constructor
+rm /home/user/projects/composite/system_binaries/cos_build-ktest/global.tests/tests.kernel_tests.global.tests
 
 set -e
 ./cos init
 ./cos build
-make -C src COMP_INTERFACES="init/stubs" COMP_IFDEPS="init/kernel" COMP_LIBDEPS="" COMP_INTERFACE=tests COMP_NAME=kernel_tests COMP_VARNAME=global.tests COMP_OUTPUT=/home/user/projects/composite/system_binaries/cos_build-test/global.tests/tests.kernel_tests.global.tests COMP_BASEADDR=0x00400000 COMP_INITARGS_FILE=/home/user/projects/composite/system_binaries/cos_build-test/global.tests/initargs_constructor.c  component
-cp system_binaries/cos_build-test/global.tests/tests.kernel_tests.global.tests system_binaries/cos_build-test/constructor
-make -C src KERNEL_OUTPUT="/home/user/projects/composite/system_binaries/cos_build-test/cos.img" CONSTRUCTOR_COMP="/home/user/projects/composite/system_binaries/cos_build-test/constructor" plat
+./cos compose composition_scripts/kernel_test.toml ktest
+#make -C src COMP_INTERFACES="init/stubs" COMP_IFDEPS="init/kernel" COMP_LIBDEPS="" COMP_INTERFACE=tests COMP_NAME=kernel_tests COMP_VARNAME=global.tests COMP_OUTPUT=/home/user/projects/composite/system_binaries/cos_build-test/global.tests/tests.kernel_tests.global.tests COMP_BASEADDR=0x00400000 COMP_INITARGS_FILE=/home/user/projects/composite/system_binaries/cos_build-test/global.tests/initargs_constructor.c  component
+#cp system_binaries/cos_build-test/global.tests/tests.kernel_tests.global.tests system_binaries/cos_build-test/constructor
+#make -C src KERNEL_OUTPUT="/home/user/projects/composite/system_binaries/cos_build-test/cos.img" CONSTRUCTOR_COMP="/home/user/projects/composite/system_binaries/cos_build-test/constructor" plat
