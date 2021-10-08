cde63bab352e5fc76ac7db24c8868735f9080849
betahxy <55125653+betahxy@users.noreply.github.com>
2021-10-08 09:36:07 -0400

    add deps setting script for 64 bit composite

---
+++ tools/set_env_deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this is used to set evnironment deps in ubuntu for 64 bit composite
+
+sudo apt install build-essential
+sudo apt install git
+sudo apt install cargo
+sudo apt install python2
+sudo apt install xorriso
+sudo apt install qemu-system-i386
