55f6b402f53dad23e2e1adc6aec8037634601ecf
betahxy <55125653+betahxy@users.noreply.github.com>
2021-10-08 12:13:09 -0400

    update env deps script

--- tools/set_env_deps.sh
+++ tools/set_env_deps.sh
@@ -2,10 +2,10 @@
 
 # this is used to set evnironment deps in ubuntu for 64 bit composite
 
-sudo apt install build-essential
-sudo apt install git
-sudo apt install cargo
-sudo apt install python2
-sudo apt install xorriso
-sudo apt install mtools
-sudo apt install qemu-system-i386
+sudo apt install build-essential -y
+sudo apt install git -y
+sudo apt install cargo -y
+sudo apt install python2 -y
+sudo apt install xorriso -y
+sudo apt install mtools -y
+sudo apt install qemu-system-i386 -y
