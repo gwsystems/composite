7e9aaeaaa6b57492692e49797d4bb51eec92ba44
betahxy <55125653+betahxy@users.noreply.github.com>
2021-10-26 11:17:08 -0400

    improve init_env script

--- tools/init_env.sh
+++ tools/init_env.sh
@@ -2,28 +2,19 @@
 
 ubuntu64()
 {
-
         # This is used to set evnironment deps in Ubuntu-64 for compile&run x86_32/x86_64 bit Composite
-        # Fully Tested Ubuntu version: 20.04
+        # Tested Ubuntu version: 18.04/20.04/21.04
         local dependencies="build-essential git cargo xorriso mtools qemu-system-i386 gcc-multilib python2.7"
         sudo apt install ${dependencies} -y
 }
 
 fedora64()
 {
-
         # This is used to set evnironment deps in Fedora/CentOS-64 for compile&run x86_32/x86_64 bit Composite
         # Tested Fedora version: 34/35
-        # FIXME: Fedora 34/35 can compile x86_32 and x86_64, but have problem running x86_32 version
+        local dependencies="g++ git cargo python2.7 xorriso mtools qemu glibc-devel.i686 libstdc++-devel.i686"
         sudo yum groupinstall "Development Tools" -y
-        sudo yum install g++ -y
-        sudo yum install git -y
-        sudo yum install cargo -y
-        sudo yum install python2 -y
-        sudo yum install xorriso -y
-        sudo yum install mtools -y
-        sudo yum install qemu -y
-        sudo yum install glibc-devel.i686 libstdc++-devel.i686 -y
+        sudo yum install ${dependencies} -y
 }
 
 usage()
