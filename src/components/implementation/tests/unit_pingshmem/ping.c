56b70efe02c68ac4a15775bd06be03b36cdd9519
evanstella <evan.p.stella@gmail.com>
2021-12-13 19:23:52 -0500

    shmem ping pong helloworld

---
+++ src/components/implementation/tests/unit_pingshmem/ping.c
@@ -0,0 +1,22 @@
+#include <cos_kernel_api.h>
+#include <cos_types.h>
+#include <ps.h>
+
+#define ITER 1024
+
+void
+cos_init(void)
+{
+
+	printc("Ping component %ld: cos_init execution\n", cos_compid());
+
+	return;
+}
+
+int
+main(void)
+{
+	printc("PING HELLO WORLD\n");
+
+	return 0;
+}
