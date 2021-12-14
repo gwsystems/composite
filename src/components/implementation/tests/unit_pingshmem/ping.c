07f119e909f3871a911b39497afb8ffa6677c0af
evanstella <evan.p.stella@gmail.com>
2021-12-14 15:25:12 -0500

    shmem testing

--- src/components/implementation/tests/unit_pingshmem/ping.c
+++ src/components/implementation/tests/unit_pingshmem/ping.c
@@ -1,22 +1,29 @@
 #include <cos_kernel_api.h>
 #include <cos_types.h>
-#include <ps.h>
+#include <cos_component.h>
+#include <cos_defkernel_api.h>
+#include <capmgr.h>
+#include <memmgr.h>
+#include <pongshmem.h>
 
-#define ITER 1024
-
-void
-cos_init(void)
-{
-
-	printc("Ping component %ld: cos_init execution\n", cos_compid());
-
-	return;
-}
+#include <string.h>
 
 int
 main(void)
 {
+	cbuf_t  id;
+	vaddr_t addr;
+
 	printc("PING HELLO WORLD\n");
+
+	id = memmgr_shared_page_allocn(1, &addr);
+	memmgr_shared_page_map(id, &addr);
+
+	PRINTLOG(PRINT_DEBUG, "%s: shared memory allocation in ping\n", (id == 0) ? "FAILURE" : "SUCCESS");
+	printc("PTR: %p\n", addr);
+
+	*((int *) addr) = 69;
+	pongshmem_read(id);
 
 	return 0;
 }
--- src/components/implementation/tests/unit_pongshmem/pong.c
+++ src/components/implementation/tests/unit_pongshmem/pong.c
@@ -1,17 +1,9 @@
 #include <cos_kernel_api.h>
 #include <cos_types.h>
 #include <ps.h>
-
-#define ITER 1024
-
-void
-cos_init(void)
-{
-
-	printc("Pong component %ld: cos_init execution\n", cos_compid());
-
-	return;
-}
+#include <capmgr.h>
+#include <memmgr.h>
+#include <pongshmem.h>
 
 int
 main(void)
@@ -19,4 +11,19 @@ main(void)
 	printc("PONG HELLO WORLD\n");
 
 	return 0;
+}
+
+void
+pongshmem_read(cbuf_t id)
+{
+	vaddr_t addr;
+
+	printc("READING....\n");
+
+	memmgr_shared_page_map(id, &addr);
+	
+	PRINTLOG(PRINT_DEBUG, "%s: shared memory mapped in pong\n", (addr == 0) ? "FAILURE" : "SUCCESS");
+
+
+	printc("MEM: %d\n", *((int *) addr));
 }
