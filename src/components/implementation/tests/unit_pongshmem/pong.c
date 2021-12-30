094c3d6312c301ebb0420c57da6e6baf7db16505
evanstella <evan.p.stella@gmail.com>
2021-12-30 17:25:54 -0500

    tabs < spaces

--- src/components/implementation/tests/unit_pongshmem/pong.c
+++ src/components/implementation/tests/unit_pongshmem/pong.c
@@ -7,55 +7,55 @@
 shm_bm_t shm;
 
 char *ping_test_strings[] = {
-	"PING TEST 1",
-	"PING TEST 2",
-	"PING TEST 3",
-	"PING TEST 4",
+    "PING TEST 1",
+    "PING TEST 2",
+    "PING TEST 3",
+    "PING TEST 4",
 };
 
 char *pong_test_strings[] = {
-	"PONG TEST 1",
-	"PONG TEST 2",
-	"PONG TEST 3",
-	"PONG TEST 4",
+    "PONG TEST 1",
+    "PONG TEST 2",
+    "PONG TEST 3",
+    "PONG TEST 4",
 };
 
 void 
 pongshmem_test_map(cbuf_t shmid)
 {
-	shm = shm_bm_map(shmid);
-	PRINTLOG(PRINT_DEBUG, "%s: Shared memory mapped in pong\n", (shm == 0) ? "FAILURE" : "SUCCESS");
+    shm = shm_bm_map(shmid);
+    PRINTLOG(PRINT_DEBUG, "%s: Shared memory mapped in pong\n", (shm == 0) ? "FAILURE" : "SUCCESS");
 }
 
 void
 pongshmem_test_objread(shm_bufid_t objid, int test_string)
 {
-	struct obj_test *obj;
-	int              failure; 
+    struct obj_test *obj;
+    int              failure; 
 
-	// get a reference to shared object sent from ping
-	obj = (struct obj_test *) shm_bm_obj_use(shm, objid);
-	PRINTLOG(PRINT_DEBUG, "%s: (%d) Pong can get shared object from buffer\n", (obj == 0) ? "FAILURE" : "SUCCESS", test_string+1);
-	
-	// verify that we can read data from ping
-	failure = strcmp(obj->string, ping_test_strings[test_string]) != 0;
-	PRINTLOG(PRINT_DEBUG, "%s: (%d) Pong can read data from ping\n", (failure) ? "FAILURE" : "SUCCESS", test_string+1);
+    // get a reference to shared object sent from ping
+    obj = (struct obj_test *) shm_bm_obj_use(shm, objid);
+    PRINTLOG(PRINT_DEBUG, "%s: (%d) Pong can get shared object from buffer\n", (obj == 0) ? "FAILURE" : "SUCCESS", test_string+1);
+    
+    // verify that we can read data from ping
+    failure = strcmp(obj->string, ping_test_strings[test_string]) != 0;
+    PRINTLOG(PRINT_DEBUG, "%s: (%d) Pong can read data from ping\n", (failure) ? "FAILURE" : "SUCCESS", test_string+1);
 
-	// send new data to ping
-	strcpy(obj->string, pong_test_strings[test_string]);
+    // send new data to ping
+    strcpy(obj->string, pong_test_strings[test_string]);
 }
 
 void
 pongshmem_test_refcnt(shm_bufid_t objid)
 {
-	struct obj_test *obj;
-	int              failure; 
-	const char      *teststr = "test string";
+    struct obj_test *obj;
+    int              failure; 
+    const char      *teststr = "test string";
 
-	// get a reference to shared object sent from ping
-	obj = (struct obj_test *) shm_bm_obj_use(shm, objid);
-	if (obj == 0) 
-		PRINTLOG(PRINT_DEBUG, "FAILURE: Pong can get shared object from buffer\n");
+    // get a reference to shared object sent from ping
+    obj = (struct obj_test *) shm_bm_obj_use(shm, objid);
+    if (obj == 0) 
+        PRINTLOG(PRINT_DEBUG, "FAILURE: Pong can get shared object from buffer\n");
 
-	shm_bm_obj_free(obj);
+    shm_bm_obj_free(obj);
 }
