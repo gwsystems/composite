0af0a7c1792976f205ed04dff93e8882191b9efc
betahxy <55125653+betahxy@users.noreply.github.com>
2022-09-03 10:07:01 -0400

    rename tcp and udp echo servers

--- src/components/implementation/simple_tcp_echo_server/simple_tcp_echo_server/simple_tcp_echo_server.c
+++ src/components/implementation/simple_tcp_echo_server/simple_tcp_echo_server/simple_tcp_echo_server.c
@@ -1,18 +1,8 @@
-
 #include <cos_types.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <netmgr.h>
 #include <netshmem.h>
-
-cbuf_t rx_shm_id;
-cbuf_t tx_shm_id;
-
-size_t rx_shmsz;
-size_t tx_shmsz;
-
-shm_bm_t rx_shm;
-shm_bm_t tx_shm;
 
 void
 cos_init(void)
@@ -28,7 +18,6 @@ cos_init(void)
 	printc("app init shm done\n");
 }
 
-#if 0
 int
 main(void)
 {
@@ -54,7 +43,6 @@ main(void)
 
 	while (1)
 	{
-		/* code */
 		objid  = netmgr_tcp_shmem_read(&data_offset, &data_len);
 		rx_obj = shm_bm_take_net_pkt_buf(netshmem_get_shm(), objid);
 		
@@ -67,44 +55,6 @@ main(void)
 		shm_bm_free_net_pkt_buf(rx_obj);
 
 		netmgr_tcp_shmem_write(objid, netshmem_get_data_offset(), data_len);
-	}
-	
-
-}
-#endif
-
-int
-main(void)
-{
-	int ret		= 0;
-	u32_t ip	= inet_addr("10.10.1.2");
-	u16_t port	= 80;
-	struct conn_addr client_addr;
-	shm_bm_objid_t           objid;
-	struct netshmem_pkt_buf *rx_obj;
-	struct netshmem_pkt_buf *tx_obj;
-	char *data;
-	u16_t data_offset, data_len;
-
-	ret = netmgr_udp_bind(ip, port);
-	assert(ret == NETMGR_OK);
-
-	while (1)
-	{
-		/* code */
-		objid  = netmgr_udp_shmem_read(&data_offset, &data_len);
-		/* application would like to own the shmem because it does not want ohters to free it. */
-		rx_obj = shm_bm_transfer_net_pkt_buf(netshmem_get_shm(), objid);
-		data = rx_obj->data + data_offset;
-
-		tx_obj = shm_bm_alloc_net_pkt_buf(netshmem_get_shm(), &objid);
-		assert(tx_obj);
-		memcpy(netshmem_get_data_buf(tx_obj), data, data_len);
-
-		/* application free unused rx buf */
-		shm_bm_free_net_pkt_buf(rx_obj);
-
-		netmgr_udp_shmem_write(objid, netshmem_get_data_offset(), data_len);
 		shm_bm_free_net_pkt_buf(tx_obj);
 	}
 }
