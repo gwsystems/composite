3d57fd8cdc160ace31ef069221ef2330b5e31e79
betahxy <noreply@noreply.github.com>
2024-05-03 01:06:00 -0400

    update netio shmem interface for read only memory

--- src/components/interface/nic_netio_shmem/nic_netio_shmem.h
+++ src/components/interface/nic_netio_shmem/nic_netio_shmem.h
@@ -7,6 +7,8 @@
 #include <shm_bm.h>
 
 void nic_netio_shmem_map(cbuf_t shm_id);
+void nic_netio_shmem_map_bifurcate(u8_t dir, cbuf_t shm_id);
 int nic_netio_shmem_bind_port(u32_t ip_addr, u16_t port);
+int nic_netio_shmem_bind_port_right(u32_t ip_addr, u16_t port, u8_t right);
 u64_t nic_netio_shmem_get_port_mac_address(u16_t port);
 #endif
---
+++ src/components/interface/nic_netio_shmem/stubs/s_stub.c
@@ -0,0 +1,6 @@
+#include <cos_stubs.h>
+#include <nic_netio_shmem.h>
+#include <cos_component.h>
+
+CWEAKSYMB int nic_netio_shmem_bind_port_right(u32_t ip_addr, u16_t port, u8_t right) { return 0; }
+CWEAKSYMB void nic_netio_shmem_map_bifurcate(u8_t dir, cbuf_t shm_id){}
