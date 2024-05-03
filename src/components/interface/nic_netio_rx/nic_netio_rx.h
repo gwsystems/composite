96dd5d67000625a5bff6cc9da131088815dcc70a
betahxy <noreply@noreply.github.com>
2024-05-03 00:32:50 -0400

    add read only interface in nicmgr

--- src/components/interface/nic_netio_rx/nic_netio_rx.h
+++ src/components/interface/nic_netio_rx/nic_netio_rx.h
@@ -8,5 +8,6 @@
 
 shm_bm_objid_t nic_netio_rx_packet(u16_t *pkt_len);
 shm_bm_objid_t nic_netio_rx_packet_batch(u8_t batch_limit);
+shm_bm_objid_t nic_netio_rx_packet_batch_ro(u8_t batch_limit);
 
 #endif
--- src/components/interface/nic_netio_rx/stubs/s_stub.c
+++ src/components/interface/nic_netio_rx/stubs/s_stub.c
@@ -5,3 +5,5 @@ COS_SERVER_3RET_STUB(shm_bm_objid_t, nic_netio_rx_packet)
 {
 	return nic_netio_rx_packet((u16_t *)r1);
 }
+
+CWEAKSYMB shm_bm_objid_t nic_netio_rx_packet_batch_ro(u8_t batch_limit) { return 0; }
