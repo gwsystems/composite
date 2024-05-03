bdce6756252f1156a578e3c76880ee825e9ec8c7
betahxy <noreply@noreply.github.com>
2024-05-03 03:09:00 -0400

    NF scheduling interface

--- src/components/interface/sched/sched.h
+++ src/components/interface/sched/sched.h
@@ -40,7 +40,9 @@ void     sched_set_tls(void* tls_addr);
 unsigned long sched_get_cpu_freq(void);
 
 thdid_t sched_thd_create(cos_thd_fn_t fn, void *data); /* lib.c */
+thdid_t sched_thd_create_nf_rx(cos_thd_fn_t fn, void *data); /* lib.c */
 thdid_t sched_thd_create_closure(thdclosure_index_t idx);
+thdid_t sched_thd_create_closure_nf_rx(thdclosure_index_t idx);
 thdid_t COS_STUB_DECL(sched_thd_create_closure)(cos_thd_fn_t fn, void *data);
 
 sched_blkpt_id_t sched_blkpt_alloc(void);
