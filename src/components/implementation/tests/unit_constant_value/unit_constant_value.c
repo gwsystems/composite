07a20e4bc5d13284f72105c09605d2752fa70f4e
mbai1010 <maorui.bai@gmail.com>
2024-08-09 12:30:48 -0400

    adjust format of if/else statement

--- src/components/implementation/tests/unit_constant_value/unit_constant_value.c
+++ src/components/implementation/tests/unit_constant_value/unit_constant_value.c
@@ -3,10 +3,10 @@
 int
 main(void)
 {
-	if (NUM_CPU == 4){
+	if (NUM_CPU == 4) {
 			printc("SUCCESS: CONSTANT VALUE(NUM_CPU) has been set to 4 \n");
 	}
-	else{
+	else {
 			printc("FAIL: CONSTANT VALUE(NUM_CPU) hasn't been changed \n");
 	}
 
